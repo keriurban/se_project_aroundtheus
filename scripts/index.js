950aa4055539dfa1f2a25e57551a9e1113a79c44
Keri Urban <theurbans1020@gmail.com>
2024-07-30 01:18:14 -0400

    adding and deleting cards, likes, previews

--- scripts/index.js
+++ scripts/index.js
@@ -25,46 +25,82 @@ const initialCards = [
   },
 ];
 
-// --------ELEMENTS
-
-const profileEditButton = document.querySelector("#profile-edit-button");
-const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileEditClose = document.querySelector("#profile-edit-close");
-const profileTitle = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
-const profileTitleInput = document.querySelector("#profile-title-input");
-const profileDescriptionInput = document.querySelector(
-  "#profile-description-input"
-);
-const profileEditForm = profileEditModal.querySelector(".modal__form");
-const cardListElement = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
+// WRAPPERS
+
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListElement = document.querySelector(".cards__list");
+const addCardModal = document.querySelector("#add-card-modal");
+const addCardForm = addCardModal.querySelector(".modal__form");
+
+const previewImageModal = document.querySelector("#preview-image-modal");
+const previewModalClose = previewImageModal.querySelector(
+  "#preview-image-close"
+);
+const modalImageElement = previewImageModal.querySelector(
+  ".modal__image_popup"
+);
+const modalCaption = previewImageModal.querySelector(".modal__preview-caption");
+const closeButtons = document.querySelectorAll(".modal__close");
+
+// BUTTONS and other DOM NODES
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditClose = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const addCardButton = document.querySelector("#add-card-button");
+const addCardClose = addCardModal.querySelector(".modal__close");
+
+// FORM DATA
+
+const profileTitleInput = profileEditForm.querySelector("#profile-title-input");
+const profileDescriptionInput = profileEditForm.querySelector(
+  "#profile-description-input"
+);
+
+const addCardTitleInput = addCardForm.querySelector("#add-card-title-input");
+const addCardUrlInput = addCardForm.querySelector("#add-card-url-input");
+
 // --------FUNCTIONS
 
-function closePopup() {
-  profileEditModal.classList.remove("modal_opened");
+function closePopup(modal) {
+  modal.classList.remove("modal_opened");
+}
+
+function openModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
 function getCardElement(cardData) {
-  // clone the template element with all its content and store it in a cardElement variable
   const cardElement = cardTemplate.cloneNode(true);
-
-  // access the card title and image and store them in variables
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardTitleElement = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const trashButton = cardElement.querySelector(".card__trash-button");
 
-  // set the path to the image to the link field of the object
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  trashButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
   cardImageElement.src = cardData.link;
-
-  // set the image alt text to the name field of the object
-  cardTitleElement.alt = cardData.name;
-
-  // set the card title to the name field of the object, too
+  cardImageElement.alt = cardData.name;
   cardTitleElement.textContent = cardData.name;
 
-  // return the ready HTML element with the filled-in data
+  cardImageElement.addEventListener("click", () => {
+    modalImageElement.src = cardData.link;
+    modalCaption.textContent = cardData.name;
+    modalImageElement.alt = cardData.name;
+    openModal(previewImageModal);
+  });
+
   return cardElement;
 }
 
@@ -74,24 +110,59 @@ function handleProfileEditSubmit(e) {
   e.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
-  closePopup();
+  closePopup(profileEditModal);
 }
 
-// --------LISTENERS
+function handleAddCardSubmit(e) {
+  e.preventDefault();
+  const name = addCardTitleInput.value;
+  const link = addCardUrlInput.value;
+  const cardElement = getCardElement({
+    name,
+    link,
+  });
+  const cardImage = cardElement.querySelector(".card__image");
+  cardListElement.prepend(cardElement);
+  addCardForm.reset();
+  closePopup(addCardModal);
+}
+
+function renderCard(cardData, wrapper) {
+  const cardElement = getCardElement(cardData);
+  wrapper.prepend(cardElement);
+}
+
+// EVENT LISTENERS
 
 profileEditButton.addEventListener("click", () => {
+  openModal(profileEditModal);
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
 profileEditClose.addEventListener("click", () => {
-  closePopup();
+  closePopup(profileEditModal);
 });
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
-initialCards.forEach((cardData) => {
-  const cardElement = getCardElement(cardData);
-  cardListElement.append(cardElement);
+addCardButton.addEventListener("click", () => {
+  openModal(addCardModal);
+  addCardModal.classList.add("modal_opened");
+});
+
+addCardClose.addEventListener("click", () => {
+  closePopup(addCardModal);
+});
+
+addCardForm.addEventListener("submit", handleAddCardSubmit);
+
+// FOR EACH
+
+initialCards.forEach((cardData) => renderCard(cardData, cardListElement));
+
+closeButtons.forEach((button) => {
+  const popup = button.closest(".modal");
+  button.addEventListener("click", () => closePopup(popup));
 });
